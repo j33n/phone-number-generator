6df758c6dc361763fb38881d5e96004b9b25776a
Jean Abayo <abagammy@gmail.com>
2019-01-31 16:00:27 +0300

    Add random number generator functionalities

--- src/pages/GenerateNumbers/GenerateNumbers.jsx
+++ src/pages/GenerateNumbers/GenerateNumbers.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const GenerateNumbers = () => (
-  <div>
-    <h2>Generate Numbers</h2>
-  </div>
-);
+import './generate.scss';
+
+class GenerateNumbers extends PureComponent {
+    state = {
+      numbersGenerated: 0,
+    }
+
+    formZeroedNumber = (number, numberOfZeroes) => {
+      let zeroes;
+      let combinedZeroes = '';
+      for (zeroes = 1; zeroes <= numberOfZeroes; zeroes += 1) {
+        combinedZeroes = `${combinedZeroes}0`;
+      }
+      return combinedZeroes + number;
+    }
+
+    stringifyNumber = (number, length) => {
+      const strNumber = number.toString(10);
+      if (strNumber.length < length) {
+        return this.formZeroedNumber(number, length - strNumber.length);
+      }
+      return number.toString(10);
+    }
+
+    triggerGenerator = () => {
+      const arrayLength = new Uint32Array(10000);
+      const randomNumbers = window.crypto.getRandomValues(arrayLength);
+      this.setState({
+        numbersGenerated: randomNumbers.length,
+      });
+
+      const generatedNumber = [];
+      randomNumbers.forEach((randomNumber) => {
+        generatedNumber.push(this.stringifyNumber(randomNumber, 10));
+      });
+    };
+
+    render() {
+      const { numbersGenerated } = this.state;
+      return (
+        <div className="container">
+          <div className="generator-button" onClick={this.triggerGenerator} type="button" role="presentation">Generate Random Phone Numbers</div>
+          {!!numbersGenerated && (
+          <span className="alert alert--success">
+            {`${numbersGenerated} Random Phone Numbers were Generated`}
+          </span>
+          )}
+        </div>
+      );
+    }
+}
 
 export default GenerateNumbers;
