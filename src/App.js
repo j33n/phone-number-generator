e4f75d2437a184c4a00fb432dbc0f73dee88e913
Jean Abayo <abagammy@gmail.com>
2019-01-31 11:22:04 +0300

    Add logo and eslint config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import logo from './assets/phone-generator-logo.png';
+import './styles/App.css';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-container">
+        <div className="header">
+          <div>Generate New Numbers</div>
+          <img src={logo} className="logo" alt="logo" />
+          <div>View All Numbers</div>
+        </div>
+        <div className="container">
+          <div className="content">Numbers</div>
+
+        </div>
       </div>
     );
   }
