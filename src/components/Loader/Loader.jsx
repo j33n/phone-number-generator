771e21fe460286b59f527d85f356ec3a3abc99ef
Jean Abayo <abagammy@gmail.com>
2019-03-27 12:23:16 +0300

    Add a f***** loader
    Remove custom loader which sucked

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,12 @@
 import React, { Fragment } from 'react';
 
 import './Loader.scss';
+import blueLoader from '../../assets/loading_blue.gif';
 
 const Loader = () => (
   <Fragment>
     <div className="loader-container">
-      <span className="a" />
-      <span className="b" />
-      <span className="c" />
+      <img src={blueLoader} alt="loader" />
     </div>
     <div className="overlay" />
   </Fragment>
